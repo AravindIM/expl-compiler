4b24cba51ac35d185bc32044a12d33d85dc049b1
Aravind I M <aravindim@yahoo.com>
2023-01-14 19:03:49 +0530

    Added: stage 2 code

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ pub enum Op {
         rhs: Box<Tnode>,
     },
     Add {
-
         lhs: Box<Tnode>,
         rhs: Box<Tnode>,
     },
@@ -25,10 +24,41 @@ pub enum Op {
 }
 
 #[derive(Debug)]
+pub struct Id {
+    pub name: String,
+}
+
+impl Id {
+    fn get_address(&self) -> Result<u32, Box<dyn Error>> {
+        Ok(self.name.chars()
+            .nth(0)
+            .ok_or(Box::<dyn Error>::from("Invalid variable"))? as u32
+            - 'a' as u32
+            + 4096)
+    }
+}
+
+#[derive(Debug)]
 pub enum Tnode {
+    NullProg,
     Op(Op),
     Num {
         value: i32,
+    },
+    Id(Id),
+    Connector {
+        lhs: Box<Tnode>,
+        rhs: Box<Tnode>,
+    },
+    Read {
+        id: Box<Tnode>,
+    },
+    Write {
+        expr: Box<Tnode>,
+    },
+    AsgStmt {
+        id: Box<Tnode>,
+        expr: Box<Tnode>,
     },
 }
 
@@ -48,10 +78,23 @@ impl RegPool {
             .pool
             .iter()
             .position(|&x| x == false)
-            .ok_or(Box::<dyn Error>::from("No free registers"))?;
+            .ok_or(Box::<dyn Error>::from("No set_free registers"))?;
         self.pool[reg] = true;
         Ok(reg)
     }
+
+    fn get_used(&self) -> Result<Vec<usize>, Box<dyn Error>> {
+        Ok(self
+            .pool
+            .iter()
+            .enumerate()
+            .filter_map(|(index, &r)| (r == true).then(|| index))
+            .collect::<Vec<usize>>())
+    }
+
+    // fn set_used(&mut self, reg: usize) {
+    //     self.pool[reg] = true;
+    // }
 
     fn set_free(&mut self, reg: usize) {
         self.pool[reg] = false;
@@ -74,7 +117,7 @@ fn write_header(out_file: &mut File) -> Result<(), Box<dyn Error>> {
     writeln!(out_file, "0")?;
     writeln!(out_file, "0")?;
     writeln!(out_file, "0")?;
-    writeln!(out_file, "MOV SP, 4095")?;
+    writeln!(out_file, "MOV SP, {}", 4096+26)?;
     Ok(())
 }
 
@@ -113,6 +156,94 @@ fn ast_to_code(
     out_file: &mut File,
 ) -> Result<usize, Box<dyn Error>> {
     match node {
+        Tnode::NullProg => return Ok(0),
+        Tnode::Id(var) => {
+            // let id = var.name;
+            let address: u32 = var.get_address()?;
+            let reg1 = regpool.get_free()?;
+            let reg2 = regpool.get_free()?;
+            writeln!(out_file, "MOV R{}, {}", reg1, address)?;
+            writeln!(out_file, "MOV R{}, [R{}]", reg2, reg1)?;
+            regpool.set_free(reg1);
+            return Ok(reg2);
+        }
+        Tnode::Connector { lhs, rhs } => {
+            let reg1 = ast_to_code(lexer, *lhs, regpool, out_file)?;
+            let reg2 = ast_to_code(lexer, *rhs, regpool, out_file)?;
+            regpool.set_free(reg1);
+            regpool.set_free(reg2);
+            let reg3 = regpool.get_free()?;
+            return Ok(reg3);
+        }
+        Tnode::Read { id } => {
+            if let Tnode::Id(var) = *id {
+                let address: u32 = var.get_address()?;
+                let used_reg_list = regpool.get_used()?;
+                for used_reg in used_reg_list.iter() {
+                    writeln!(out_file, "PUSH R{}", used_reg)?;
+                }
+                let reg1 = regpool.get_free()?;
+                let reg2 = regpool.get_free()?;
+                writeln!(out_file, "MOV R{}, \"Read\"", reg1)?;
+                writeln!(out_file, "PUSH R{}", reg1)?;
+                writeln!(out_file, "MOV R{}, -1", reg1)?;
+                writeln!(out_file, "PUSH R{}", reg1)?;
+                writeln!(out_file, "MOV R{}, {}", reg1, address)?;
+                writeln!(out_file, "PUSH R{}", reg1)?;
+                writeln!(out_file, "PUSH R{}", reg2)?;
+                writeln!(out_file, "PUSH R{}", reg2)?;
+                writeln!(out_file, "CALL 0")?;
+                writeln!(out_file, "POP R{}", reg1)?;
+                writeln!(out_file, "POP R{}", reg2)?;
+                writeln!(out_file, "POP R{}", reg2)?;
+                writeln!(out_file, "POP R{}", reg2)?;
+                writeln!(out_file, "POP R{}", reg2)?;
+                regpool.set_free(reg2);
+                for used_reg in used_reg_list.iter() {
+                    writeln!(out_file, "POP R{}", used_reg)?;
+                }
+                return Ok(reg1);
+            }
+            let reg1 = regpool.get_free()?;
+            return Ok(reg1);
+        }
+        Tnode::Write { expr } => {
+            let reg1 = ast_to_code(lexer, *expr, regpool, out_file)?;
+            let used_reg_list = regpool.get_used()?;
+            for used_reg in used_reg_list.iter() {
+                writeln!(out_file, "PUSH R{}", used_reg)?;
+            }
+            let reg2 = regpool.get_free()?;
+            writeln!(out_file, "MOV R{}, \"Write\"", reg2)?;
+            writeln!(out_file, "PUSH R{}", reg2)?;
+            writeln!(out_file, "MOV R{}, -2", reg2)?;
+            writeln!(out_file, "PUSH R{}", reg2)?;
+            writeln!(out_file, "PUSH R{}", reg1)?;
+            writeln!(out_file, "PUSH R{}", reg2)?;
+            writeln!(out_file, "PUSH R{}", reg2)?;
+            writeln!(out_file, "CALL 0")?;
+            writeln!(out_file, "POP R{}", reg1)?;
+            writeln!(out_file, "POP R{}", reg2)?;
+            writeln!(out_file, "POP R{}", reg2)?;
+            writeln!(out_file, "POP R{}", reg2)?;
+            writeln!(out_file, "POP R{}", reg2)?;
+            regpool.set_free(reg2);
+            for used_reg in used_reg_list.iter() {
+                writeln!(out_file, "POP R{}", used_reg)?;
+            }
+            return Ok(reg1);
+        }
+        Tnode::AsgStmt { id, expr } => {
+            if let Tnode::Id(var) = *id {
+                let address = var.get_address()?;
+                let reg1 = ast_to_code(lexer, *expr, regpool, out_file)?;
+                let reg2 = regpool.get_free()?;
+                writeln!(out_file, "MOV R{}, {}", reg2, address)?;
+                writeln!(out_file, "MOV [R{}], R{}", reg2, reg1)?;
+            };
+            let reg3 = regpool.get_free()?;
+            return Ok(reg3);
+        }
         Tnode::Op(operator) => match operator {
             Op::Div { lhs, rhs } => {
                 let reg1 = ast_to_code(lexer, *lhs, regpool, out_file)?;
--- src/yacc.y
+++ src/yacc.y
@@ -1,15 +1,43 @@
-%start Expr
+%start Prog
 
 /* %epp ADD "+" */
 /* %epp SUB "-" */
 /* %epp MUL "*" */
 /* %epp DIV "/" */
 
-%token "(" ")"
+%token "BEGIN" "END" "ENDSTMT" "ID" "ASG" "READ" "WRITE" "NUM" "(" ")" "UNMATCHED"
 %left "ADD" "SUB"
 %left "MUL" "DIV"
 
 %%
+
+Prog -> Result<Tnode, ()>:
+        "BEGIN" Slist "END" { $2 }
+      | "BEGIN" "END" { Ok( Tnode::NullProg ) }
+      ;
+
+Slist -> Result<Tnode, ()>:
+        Slist Stmt { Ok( Tnode::Connector{ lhs: Box::new($1?), rhs: Box::new($2?) } ) }
+      | Stmt { $1 }
+      ;
+
+Stmt -> Result<Tnode, ()>:
+        InputStmt { $1 }
+      | OutputStmt { $1 }
+      | AsgStmt { $1 }
+      ;
+
+InputStmt -> Result<Tnode, ()>:
+             "READ" "(" Id ")" "ENDSTMT" { Ok( Tnode::Read{ id: Box::new($3?) } ) }
+            ;
+
+OutputStmt -> Result<Tnode, ()>:
+              "WRITE" "(" Expr ")" "ENDSTMT" { Ok( Tnode::Write{ expr: Box::new($3?) } ) }
+             ;
+
+AsgStmt -> Result<Tnode, ()>:
+           Id "ASG" Expr "ENDSTMT" { Ok( Tnode::AsgStmt{ id: Box::new($1?), expr: Box::new($3?)} ) }
+           ;
 
 Expr -> Result<Tnode, ()>:
         "(" Expr ")" { $2 }
@@ -18,9 +46,16 @@ Expr -> Result<Tnode, ()>:
       |	Expr "ADD" Expr { Ok( Tnode::Op(Op::Add{ lhs: Box::new($1?), rhs: Box::new($3?)} ) ) }
       |	Expr "SUB" Expr { Ok( Tnode::Op(Op::Sub{ lhs: Box::new($1?), rhs: Box::new($3?)} ) ) }
       | "NUM" { Ok( Tnode::Num{ value: $lexer.span_str($1.as_ref().unwrap().span()).parse::<i32>().expect("Invalid number") } ) }
+      | Id { $1 }
       ;
 
+Id -> Result<Tnode, ()>:
+      "ID" { Ok( Tnode::Id(Id{ name: $lexer.span_str($1.as_ref().unwrap().span()).to_string() }) ) }
+      ;
+
+Unmatched -> ():
+             "UNMATCHED" { }
+             ;
 %%
 // Any functions here are in scope for all the grammar actions above.
-use compiler::Tnode;
-use compiler::Op;
+use compiler::*;
